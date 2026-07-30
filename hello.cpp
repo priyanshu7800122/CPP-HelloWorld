bbaf72147cad432f80070f2b4af36d3f56ad391b
Priyanshu Kumari <priyanshaa544@gmail.com>
2026-07-30 09:57:35 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+// cpp program 
+/// comments are used 
+// # preprocessor
+// include preprocessor 
+//io stram is header file 
+// multi line comments 
+
+
+
+#include<iostream>
+using namespace std;
+
+// functions takes some data using (arguments ) and return data using return statement 
+// main is a system defined function where cpp program is execution starts 
+int main(){
+    cout<< "hello there is our first cpp program";
+    return 0;
+}
+
+//to execute this code we need the compiler window based compiler 
+//window based compiler 
+
